a76f6b32b3d71416cc9eb4acfff24ec2294819f8
mas <mas.sam1368@gmail.com>
2020-11-15 21:07:10 +0330

    Ex2

--- Week1/js-exercises/ex2-aboutMe.js
+++ Week1/js-exercises/ex2-aboutMe.js
@@ -9,3 +9,18 @@
  5. See HTML
  6. Create a new img element and set its src attribute to a picture of you.Append that element to the page.
  */
+
+ document.body.style.fontFamily = "Arial, sans-serif";
+ document.getElementById('nickname').textContent = 'Masoud';
+ document.getElementById('fav-food').textContent = 'Pasta';
+ document.getElementById('hometown').textContent = 'Kerman';
+
+ selectList = document.querySelectorAll('li');
+for(let i = 0 ; i < selectList.length ; i++) {
+  selectList[i].className = 'list-item';
+}
+
+const imgElement = document.createElement('img');
+imgElement.src = 'https://www.kindpng.com/picc/m/22-223941_transparent-avatar-png-male-avatar-icon-transparent-png.png';
+imgElement.alt = 'avatar';
+document.body.appendChild(imgElement);
