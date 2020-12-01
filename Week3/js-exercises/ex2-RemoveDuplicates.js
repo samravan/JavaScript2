6826f59aa621e52dffa883739d243b6c2e5f70d7
mas <mas.sam1368@gmail.com>
2020-12-01 17:41:38 +0330

    Ex2

--- Week3/js-exercises/ex2-RemoveDuplicates.js
+++ Week3/js-exercises/ex2-RemoveDuplicates.js
@@ -1,35 +1,19 @@
-/**
- 
- ** Exercise 2: The lottery machine **
- Write a function called removeDuplicates. This function accept an array as an argument
- does not return anything but removes any duplicate elements from the array.
+const letters = ['a', 'b', 'b', 'c', 'd', 'a', 'e', 'f', 'f', 'c', 'b'];
+const nonDuplicates = [];
 
- The function should remove duplicate elements. So the result should be:
- ['a', 'b', 'c', 'd', 'e', 'f']
-
- */
-
-/**
- * Checks your solution against correct solution
- * @param {Array} array your solution
- * @returns boolean
- */
-function checkSolution(array) {
-  const solution = ['a', 'b', 'c', 'd', 'e', 'f'];
-  if (array == null) return false;
-  if (array.length !== solution.length) return false;
-
-  for (let i = 0; i < solution.length; i++) {
-    if (array[i] !== solution[i]) return false;
+function removeDuplicates(array) {
+  nonDuplicates.push(array[0]);
+  for (let i = 1; i < array.length; i++) {
+    const x = [];
+    for (let j = 0; j < i; j++) {
+      if (array[j] == array[i]) {
+        x.push(array[j]);
+      }
+    }
+    if (x.length == 0) {
+      nonDuplicates.push(array[i]);
+    }
   }
-  return true;
+  return nonDuplicates;
 }
-
-// WRITE YOUR FUNCTION HERE
-
-const letters = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c', 'b'];
-removeDuplicates(letters);
-
-if (checkSolution(letters)) {
-  console.log("Hooray!");
-}
+console.log(removeDuplicates(letters));
