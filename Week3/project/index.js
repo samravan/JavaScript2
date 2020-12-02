a83d2b98f0f43fd99bbd9fa0558829609e89dbc5
mas <mas.sam1368@gmail.com>
2020-12-03 01:10:54 +0330

    Finished project

--- Week3/project/index.js
+++ Week3/project/index.js
@@ -1,3 +1,25 @@
-// Your code goes in here
+const bill = document.getElementById('bill');
+const service = document.getElementById('service');
+const people = document.getElementById('people');
+const calculate = document.getElementById('calculate');
+const amount = document.getElementById('amount');
+const each = document.getElementById('each');
 
-document.querySelector("#app").innerText = "Tip Calculator";
+calculate.addEventListener("click", calculateTip);
+
+function calculateTip(e) {
+  e.preventDefault();
+
+
+  if (bill.value == '' || service.value == 'default' || people.value == '') {
+    alert('Please fill all fields!');
+  } else {
+    amount.textContent = parseFloat(service.value * bill.value / 100 / people.value).toFixed(2);
+    each.style.display = 'block';
+
+    if (people.value == 1) {
+      each.style.display = 'none';
+    }
+  }
+
+}
